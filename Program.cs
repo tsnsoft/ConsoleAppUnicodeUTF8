e48b229b796092050cf1838113de77040d025d51
proffix4 <proffix4@gmail.com>
2022-08-01 18:17:49 +0600

    update

--- Program.cs
+++ Program.cs
@@ -9,5 +9,6 @@ if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
 Console.WriteLine("Замечательно! Das ist großartig! Wonderful! 精彩的！ رائع! ");
 var line = Console.ReadLine();
 Console.WriteLine(line);
-Console.Write("Press any key to continue . . . ");
-Console.ReadKey(true);
+
+//Console.Write("Press any key to continue . . . ");
+//Console.ReadKey(true);
