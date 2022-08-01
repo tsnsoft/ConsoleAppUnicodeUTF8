5245ed2542ac25857c5bb324532ffd43450ea32d
proffix4 <proffix4@gmail.com>
2022-08-01 15:55:34 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+{
+    Console.InputEncoding = Encoding.Unicode;
+    Console.OutputEncoding = Encoding.Unicode;
+}
+Console.WriteLine("Замечательно! Das ist großartig! Wonderful! 精彩的！ رائع! ");
+var line = Console.ReadLine();
+Console.WriteLine(line);
+Console.Write("Press any key to continue . . . ");
+Console.ReadKey(true);
